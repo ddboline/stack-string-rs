0ad16719c00aa8e0671836106116cb65cbe65c5e
Daniel Boline <ddboline@gmail.com>
2020-07-16 19:10:24 -0400

    use Serialize, Deserialize from SmartString crate

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,12 @@
 #[macro_use]
 extern crate diesel;
 
-use anyhow::Error;
-use derive_more::{Deref, DerefMut, From, Index, IndexMut, Into};
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
+use std::convert::Infallible;
+use derive_more::{Deref, DerefMut, Display, From, Index, IndexMut, Into};
+use serde::{Deserialize, Serialize};
 use smartstring::alias::String as SmartString;
 use std::{
     borrow::{Borrow, Cow},
-    fmt::{self, Display, Formatter},
     iter::FromIterator,
     path::Path,
     str::FromStr,
@@ -33,6 +32,9 @@ use tokio_postgres::types::{FromSql, IsNull, ToSql, Type};
 
 #[cfg(feature = "diesel_types")]
 #[derive(
+    Display,
+    Serialize,
+    Deserialize,
     Deref,
     DerefMut,
     Index,
@@ -55,6 +57,9 @@ pub struct StackString(SmartString);
 
 #[cfg(not(feature = "diesel_types"))]
 #[derive(
+    Display,
+    Serialize,
+    Deserialize,
     Debug,
     Clone,
     Into,
@@ -90,54 +95,6 @@ impl StackString {
     }
 }
 
-impl Serialize for StackString {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        serializer.serialize_str(self.0.as_str())
-    }
-}
-
-impl<'de> Deserialize<'de> for StackString {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        use ::serde::de::{Error, Visitor};
-
-        struct SmartVisitor;
-
-        impl<'a> Visitor<'a> for SmartVisitor {
-            type Value = StackString;
-
-            fn expecting(&self, formatter: &mut Formatter) -> fmt::Result {
-                formatter.write_str("a string")
-            }
-
-            fn visit_str<E: Error>(self, v: &str) -> Result<Self::Value, E> {
-                Ok(v.into())
-            }
-
-            fn visit_borrowed_str<E: Error>(self, v: &'a str) -> Result<Self::Value, E> {
-                Ok(v.into())
-            }
-
-            fn visit_string<E: Error>(self, v: String) -> Result<Self::Value, E> {
-                Ok(v.into())
-            }
-        }
-
-        deserializer.deserialize_str(SmartVisitor)
-    }
-}
-
-impl Display for StackString {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.as_str())
-    }
-}
-
 impl From<StackString> for String {
     fn from(item: StackString) -> Self {
         item.0.into()
@@ -152,7 +109,7 @@ impl From<String> for StackString {
 
 impl From<&String> for StackString {
     fn from(item: &String) -> Self {
-        Self(item.as_str().into())
+        Self(item.into())
     }
 }
 
@@ -168,77 +125,15 @@ impl Borrow<str> for StackString {
     }
 }
 
-#[cfg(feature = "postgres_types")]
-impl<'a> FromSql<'a> for StackString {
-    fn from_sql(
-        ty: &Type,
-        raw: &'a [u8],
-    ) -> Result<Self, Box<dyn std::error::Error + 'static + Send + Sync>> {
-        let s = <String as FromSql>::from_sql(ty, raw)?;
-        Ok(s.into())
-    }
-
-    fn accepts(ty: &Type) -> bool {
-        <String as FromSql>::accepts(ty)
-    }
-}
-
-#[cfg(feature = "postgres_types")]
-impl ToSql for StackString {
-    fn to_sql(
-        &self,
-        ty: &Type,
-        out: &mut BytesMut,
-    ) -> Result<IsNull, Box<dyn std::error::Error + Sync + Send>>
-    where
-        Self: Sized,
-    {
-        ToSql::to_sql(&self.as_str(), ty, out)
-    }
-
-    fn accepts(ty: &Type) -> bool
-    where
-        Self: Sized,
-    {
-        <String as ToSql>::accepts(ty)
-    }
-
-    fn to_sql_checked(
-        &self,
-        ty: &Type,
-        out: &mut BytesMut,
-    ) -> Result<IsNull, Box<dyn std::error::Error + Sync + Send>> {
-        self.as_str().to_sql_checked(ty, out)
-    }
-}
-
-#[cfg(feature = "diesel_types")]
-impl<DB> DeToSql<Text, DB> for StackString
-where
-    DB: Backend,
-    str: DeToSql<Text, DB>,
-{
-    fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> SerResult {
-        self.as_str().to_sql(out)
-    }
-}
-
-#[cfg(feature = "diesel_types")]
-impl<ST, DB> DeFromSql<ST, DB> for StackString
-where
-    DB: Backend,
-    *const str: DeFromSql<ST, DB>,
-{
-    fn from_sql(bytes: Option<&DB::RawValue>) -> DeResult<Self> {
-        let str_ptr = <*const str as DeFromSql<ST, DB>>::from_sql(bytes)?;
-        let string = unsafe { &*str_ptr };
-        Ok(string.into())
-    }
-}
-
 impl AsRef<str> for StackString {
     fn as_ref(&self) -> &str {
-        self.as_str()
+        self.0.as_str()
+    }
+}
+
+impl AsRef<[u8]> for StackString {
+    fn as_ref(&self) -> &[u8] {
+        self.0.as_ref()
     }
 }
 
@@ -249,7 +144,7 @@ impl AsRef<Path> for StackString {
 }
 
 impl FromStr for StackString {
-    type Err = Error;
+    type Err = Infallible;
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         Ok(s.into())
     }
@@ -288,5 +183,73 @@ impl FromIterator<char> for StackString {
         let mut buf = Self::new();
         buf.0.extend(iter);
         buf
+    }
+}
+
+#[cfg(feature = "postgres_types")]
+impl<'a> FromSql<'a> for StackString {
+    fn from_sql(
+        ty: &Type,
+        raw: &'a [u8],
+    ) -> Result<Self, Box<dyn std::error::Error + 'static + Send + Sync>> {
+        let s = <&'a str as FromSql>::from_sql(ty, raw)?;
+        Ok(s.into())
+    }
+
+    fn accepts(ty: &Type) -> bool {
+        <&'a str as FromSql>::accepts(ty)
+    }
+}
+
+#[cfg(feature = "postgres_types")]
+impl ToSql for StackString {
+    fn to_sql(
+        &self,
+        ty: &Type,
+        out: &mut BytesMut,
+    ) -> Result<IsNull, Box<dyn std::error::Error + Sync + Send>>
+    where
+        Self: Sized,
+    {
+        ToSql::to_sql(&self.as_str(), ty, out)
+    }
+
+    fn accepts(ty: &Type) -> bool
+    where
+        Self: Sized,
+    {
+        <String as ToSql>::accepts(ty)
+    }
+
+    fn to_sql_checked(
+        &self,
+        ty: &Type,
+        out: &mut BytesMut,
+    ) -> Result<IsNull, Box<dyn std::error::Error + Sync + Send>> {
+        self.as_str().to_sql_checked(ty, out)
+    }
+}
+
+#[cfg(feature = "diesel_types")]
+impl<ST, DB> DeFromSql<ST, DB> for StackString
+where
+    DB: Backend,
+    *const str: DeFromSql<ST, DB>,
+{
+    fn from_sql(bytes: Option<&DB::RawValue>) -> DeResult<Self> {
+        let str_ptr = <*const str as DeFromSql<ST, DB>>::from_sql(bytes)?;
+        let s = unsafe { &*str_ptr };
+        Ok(s.into())
+    }
+}
+
+#[cfg(feature = "diesel_types")]
+impl<DB> DeToSql<Text, DB> for StackString
+where
+    DB: Backend,
+    str: DeToSql<Text, DB>,
+{
+    fn to_sql<W: Write>(&self, out: &mut Output<W, DB>) -> SerResult {
+        self.as_str().to_sql(out)
     }
 }
