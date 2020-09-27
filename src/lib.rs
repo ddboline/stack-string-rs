966257c4f5660c4ddc769dcd831b4d1c84d3c47d
Daniel Boline <ddboline@gmail.com>
2020-09-27 12:23:04 -0400

    clippy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+#![allow(clippy::must_use_candidate)]
+#![allow(clippy::too_many_lines)]
+#![allow(clippy::module_name_repetitions)]
+#![allow(clippy::cast_precision_loss)]
+#![allow(clippy::cast_sign_loss)]
+#![allow(clippy::cast_possible_truncation)]
+#![allow(clippy::cast_possible_wrap)]
+#![allow(clippy::similar_names)]
+#![allow(clippy::shadow_unrelated)]
+#![allow(clippy::missing_errors_doc)]
+#![allow(clippy::used_underscore_binding)]
+
 #[cfg(feature = "diesel_types")]
 #[macro_use]
 extern crate diesel;
